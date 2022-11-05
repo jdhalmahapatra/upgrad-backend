35d6a18f4ae616588dc5401629cc56bc9193d1d1
jdhalmahapatra <jyotiraditya12@hotmail.com>
2022-11-05 17:31:43 +0530

    push the Java code

---
+++ src/main/java/com/upgrad/bookmyconsultation/entity/Rating.java
@@ -0,0 +1,75 @@
+package com.upgrad.bookmyconsultation.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+
+//mark this class as an 'entity class'
+//Use Data annotation to generate boilerplate code
+//Use NoArgsConstructor annotation
+//create a class name Rating
+	//create a primary key called 'id' of type String
+	//initialise id with a UUID using UUID.randomUUID
+	//create appointmentId of type String
+	//create doctorId of type String
+	//create rating of type Integer
+	//create comments of type String
+	//Set access modifiers for all these members to 'private'
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating{
+	@Id
+	private String id = UUID.randomUUID().toString();
+	
+	private String appointmentId;
+	private String doctorId;
+	private int rating;
+	private String comments;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	public String getDoctorId() {
+		return doctorId;
+	}
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/upgrad/bookmyconsultation/entity/User.java
@@ -0,0 +1,95 @@
+package com.upgrad.bookmyconsultation.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+
+//Mark it with Data, Entity, Builder, NoArgsConstructor, AllArgsConstructor
+//create a class named User
+	//create firstName of type String
+	//create lastName of type String
+	//create dob of type String
+	//create mobile of type String
+	//create primary key 'emailId' of type String
+	//create password of type String
+	//create createdDate of type String
+	//create salt of type String
+	//all the mentioned members must be private
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User{
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private String mobile;
+	
+	@Id
+	private String emailId;
+	
+	private String password;
+	private String createDate;
+	private String salt;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	
+	
+	
+}
